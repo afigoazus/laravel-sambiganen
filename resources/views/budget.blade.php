e01f184880d8b916d200be65a30689184da7fa7d
Afigo04 <afigomen@gmail.com>
2025-02-05 21:27:17 +0700

    pembuatan tampilan galeri

--- resources/views/budget.blade.php
+++ resources/views/budget.blade.php
@@ -11,58 +11,63 @@
     <section class="mx-auto w-11/12 mt-8">
         <!-- menu button -->
         <div class="flex justify-between">
-            <div>
+            <div class="text-xl">
                 <p>Info Grafik APBD</p>
                 <p>Realisasi Tahun Anggaran 2024</p>
             </div>
-            <button class="bg-primary text-white px-5 rounded-xl">2024</button>
+            <button class="bg-primary text-white px-4 rounded-xl">2024</button>
         </div>
         <!-- menu button end -->
 
         <!-- pendapatan -->
         <div class="mt-8">
-            <h2 class="text-center text-xl text-secondary">Pendapatan</h2>
-            <!-- pendapatan asli -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pendapatan Asli Desa</p>
-                <p class="text-center text-2xl">Rp. 181,800,000.00</p>
-            </div>
-            <!-- pendapatan asli end -->
+            <h2 class="text-center text-2xl text-secondary">Pendapatan</h2>
 
-            <!-- dana desa -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Dana Desa</p>
-                <p class="text-center text-2xl">Rp. 1,235,203,000.00</p>
-            </div>
-            <!-- dana desa end -->
+            <!-- pendapatan content -->
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <!-- pendapatan asli -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pendapatan Asli Desa</p>
+                    <p class="text-center text-2xl">Rp. 181,800,000.00</p>
+                </div>
+                <!-- pendapatan asli end -->
 
-            <!-- alokasi dana -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Alokasi Dana Desa</p>
-                <p class="text-center text-2xl">Rp. 779,650,000.00</p>
-            </div>
-            <!-- alokasi dana end -->
+                <!-- dana desa -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Dana Desa</p>
+                    <p class="text-center text-2xl">Rp. 1,235,203,000.00</p>
+                </div>
+                <!-- dana desa end -->
+
+                <!-- alokasi dana -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Alokasi Dana Desa</p>
+                    <p class="text-center text-2xl">Rp. 779,650,000.00</p>
+                </div>
+                <!-- alokasi dana end -->
+                
+                <!-- bantuan keuangan -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bantuan Keuangan Kabupaten</p>
+                    <p class="text-center text-2xl">Rp. 0.00</p>
+                </div>
+                <!-- bantuan keungan end -->
             
-            <!-- bantuan keuangan -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bantuan Keuangan Kabupaten</p>
-                <p class="text-center text-2xl">Rp. 0.00</p>
+                <!-- bagi hasil -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Penerima Bagi Hasil Pajak Dan Retribusi</p>
+                    <p class="text-center text-2xl">Rp. 0.00</p>
+                </div>
+                <!-- bagi hasil end -->
+                
+                <!-- lain-lain -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Lain-lain</p>
+                    <p class="text-center text-2xl">Rp. 0.00</p>
+                </div>
+                <!-- lain-lain end -->
             </div>
-            <!-- bantuan keungan end -->
-           
-            <!-- bagi hasil -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Penerima Bagi Hasil Pajak Dan Retribusi</p>
-                <p class="text-center text-2xl">Rp. 0.00</p>
-            </div>
-            <!-- bagi hasil end -->
-            
-            <!-- lain-lain -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Lain-lain</p>
-                <p class="text-center text-2xl">Rp. 0.00</p>
-            </div>
-            <!-- lain-lain end -->
+            <!-- pendapatan content end -->
             
             <!-- total -->
             <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
@@ -75,35 +80,40 @@
 
         <!-- belanja -->
         <div class="mt-8">
-            <h2 class="text-center text-xl text-secondary">Belanja</h2>
-            <!-- bidang penyelenggaraan -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Penyelenggaraan Pemerintah Desa</p>
-                <p class="text-center text-2xl">Rp. 751,684,766.95</p>
-            </div>
-            <!-- bidang penyelenggaraan end -->
+            <h2 class="text-center text-2xl text-secondary">Belanja</h2>
 
-            <!-- bidang pelaksaan -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Pelaksaan Pembangunan Desa</p>
-                <p class="text-center text-2xl">Rp. 682,053,000.00</p>
-            </div>
-            <!-- bidang pelaksaan end -->
-                 
-            <!-- bidang pemberdayaan -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Pembinaan Kemasyarakatan</p>
-                <p class="text-center text-2xl">Rp. 349,727,064.55</p>
-            </div>
-            <!-- bidang pemberdayaan end -->
+            <!-- belanja content -->
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <!-- bidang penyelenggaraan -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Penyelenggaraan Pemerintah Desa</p>
+                    <p class="text-center text-2xl">Rp. 751,684,766.95</p>
+                </div>
+                <!-- bidang penyelenggaraan end -->
 
-            <!-- bidang bencana -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Penanggulangan Bencana Darurat dan Mendesak Desa</p>
-                <p class="text-center text-2xl">Rp. 152,642,224.00</p>
+                <!-- bidang pelaksaan -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Pelaksaan Pembangunan Desa</p>
+                    <p class="text-center text-2xl">Rp. 682,053,000.00</p>
+                </div>
+                <!-- bidang pelaksaan end -->
+                    
+                <!-- bidang pemberdayaan -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Pembinaan Kemasyarakatan</p>
+                    <p class="text-center text-2xl">Rp. 349,727,064.55</p>
+                </div>
+                <!-- bidang pemberdayaan end -->
+
+                <!-- bidang bencana -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full sm:col-span-3 sm:w-4/5">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bidang Penanggulangan Bencana Darurat dan Mendesak Desa</p>
+                    <p class="text-center text-2xl">Rp. 152,642,224.00</p>
+                </div>
+                <!-- bidang bencana end -->
             </div>
-            <!-- bidang bencana end -->
-            
+            <!-- belanja content end -->
+
             <!-- jumlah belanja -->
             <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
                 <p class="bg-[#F07167] text-white p-1 rounded-t-lg w-auto">Jumlah Belanja</p>
@@ -111,92 +121,111 @@
             </div>
             <!-- jumlah belanja end -->
 
-            <!-- surplus -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Surplus/Defisit</p>
-                <p class="text-center text-2xl">Rp. (59,954,055,50)</p>
-            </div>
-            <!-- surplus end -->
+            <!-- belanja 2 -->
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <!-- surplus -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Surplus/Defisit</p>
+                    <p class="text-center text-2xl">Rp. (59,954,055,50)</p>
+                </div>
+                <!-- surplus end -->
 
-            <!-- Penerimaan pembiayaan silpa -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Penerimaan Pembiayaan Penyertaan Modal Desa</p>
-                <p class="text-center text-2xl">Rp. 64,954,055.50</p>
-            </div>
-            <!-- penerimaan pembiayaan silpa end -->
-             
-            <!-- penerima pembiayaan modal -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Penerima Pembiayaan Penyertaan Modal Desa</p>
-                <p class="text-center text-2xl">Rp. 5,000,000.00</p>
-            </div>
-            <!-- penerima pembiayaan modal end -->
+                <!-- Penerimaan pembiayaan silpa -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Penerimaan Pembiayaan Penyertaan Modal Desa</p>
+                    <p class="text-center text-2xl">Rp. 64,954,055.50</p>
+                </div>
+                <!-- penerimaan pembiayaan silpa end -->
+                
+                <!-- penerima pembiayaan modal -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Penerima Pembiayaan Penyertaan Modal Desa</p>
+                    <p class="text-center text-2xl">Rp. 5,000,000.00</p>
+                </div>
+                <!-- penerima pembiayaan modal end -->
 
-            <!-- pembiayaan netto -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pembiayaan Netto</p>
-                <p class="text-center text-2xl">Rp. 59,954,055.50</p>
+                <!-- pembiayaan netto -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-4/5 sm:col-span-3">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pembiayaan Netto</p>
+                    <p class="text-center text-2xl">Rp. 59,954,055.50</p>
+                </div>
+                <!-- pembiayaan netto end -->
             </div>
-            <!-- pembiayaan netto end -->
+            <!-- belanja 2 end -->
+
+            
         </div>
         <!-- belanja end -->
         
         <!-- prioritas dana desa -->
         <div class="mt-8"> 
-            <h2 class="text-center text-xl text-secondary">Prioritas Dana Desa</h2> 
-            <!-- bantuan langsung -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bantuan Langsung Tunai Desa</p>
-                <p class="text-center text-2xl">Rp. 144,000,000.00</p>
-            </div>
-            <!-- bantuan langsung end -->
+            <h2 class="text-center text-2xl text-secondary">Prioritas Dana Desa</h2> 
+            
 
-            <!-- ketahanan pangan -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Ketahanan Pangan</p>
-                <p class="text-center text-2xl">Rp. 260,000,000.00</p>
-            </div>
-            <!-- ketahanan pangan end -->
+            <!-- prioritas dana desa content -->
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <!-- bantuan langsung -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Bantuan Langsung Tunai Desa</p>
+                    <p class="text-center text-2xl">Rp. 144,000,000.00</p>
+                </div>
+                <!-- bantuan langsung end -->
 
-            <!-- konvergensi stanting -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Konvergensi Stanting</p>
-                <p class="text-center text-2xl">Rp. 45,250,000.00</p>
+                <!-- ketahanan pangan -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Ketahanan Pangan</p>
+                    <p class="text-center text-2xl">Rp. 260,000,000.00</p>
+                </div>
+                <!-- ketahanan pangan end -->
+
+                <!-- konvergensi stanting -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Konvergensi Stanting</p>
+                    <p class="text-center text-2xl">Rp. 45,250,000.00</p>
+                </div>
+                <!-- korvengensi stanting end -->
             </div>
-            <!-- korvengensi stanting end -->
+            <!-- prioritas dana desa content end -->
+
+            
         </div>
         <!-- prioritas desa end -->
 
         <!-- rencana kegiatan pembangunan -->
         <div class="my-8">
-            <h2 class="text-center text-xl text-secondary">Rencana Kegiatan Pembangunan <br> Infrastruktur</h2>
-            <!-- pavingasi jalan rt 01 rw 01 -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pavingasi Jalan RT 01 RW 01 Dukuh Krajan</p>
-                <p class="text-center text-2xl">Rp. 25,000,000.00</p>
-            </div>
-            <!-- pavingansi jalan rt 01 rw 01 end -->
+            <h2 class="text-center text-2xl text-secondary">Rencana Kegiatan Pembangunan <br> Infrastruktur</h2>
 
-            <!-- pavingasi jalan rt 05 rw 01 -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pavingasi Jalan RT 05 Rw 01 Dukuh Krajan</p>
-                <p class="text-center text-2xl">Rp. 75,000,000.00</p>
-            </div>
-            <!-- pavingasi jalan rt 05 rw 01 end -->
+            <!-- rencana kegiatan content -->
+            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <!-- pavingasi jalan rt 01 rw 01 -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pavingasi Jalan RT 01 RW 01 Dukuh Krajan</p>
+                    <p class="text-center text-2xl">Rp. 25,000,000.00</p>
+                </div>
+                <!-- pavingansi jalan rt 01 rw 01 end -->
 
-            <!-- pemeliharaan kantor -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pemeliharaan/Reham Kantor Desa</p>
-                <p class="text-center text-2xl">Rp. 100,000,000.00</p>
+                <!-- pavingasi jalan rt 05 rw 01 -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pavingasi Jalan RT 05 Rw 01 Dukuh Krajan</p>
+                    <p class="text-center text-2xl">Rp. 75,000,000.00</p>
+                </div>
+                <!-- pavingasi jalan rt 05 rw 01 end -->
+
+                <!-- pemeliharaan kantor -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Pemeliharaan/Reham Kantor Desa</p>
+                    <p class="text-center text-2xl">Rp. 100,000,000.00</p>
+                </div>
+                <!-- pemeliharaan kantor end -->
+                
+                <!-- cadangan boss -->
+                <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto sm:w-full sm:col-span-3">
+                    <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Cadangan boss</p>
+                    <p class="text-center text-2xl">Rp. -</p>
+                </div>
+                <!-- cadangan boss end -->
             </div>
-            <!-- pemeliharaan kantor end -->
-            
-            <!-- cadangan boss -->
-            <div class="bg-slate-50 my-5 rounded-xl w-4/5 mx-auto">
-                <p class="bg-secondary text-white p-1 rounded-t-lg w-auto">Cadangan boss</p>
-                <p class="text-center text-2xl">Rp. -</p>
-            </div>
-            <!-- cadangan boss end -->
+            <!-- rencana kegiatan content end -->
         </div>
     </section>
 </main>
